cbcf3d7d80150bdca49dca78efa136457cb19369
Carlos Pereyra <Revolver@Joy.local>
2020-01-14 22:32:11 -0800

    problem 6 python script

---
+++ HW1/Commutation_x_p_psi.py
@@ -0,0 +1,44 @@
+#                     Chem 210A  C.Z. Pereyra
+#                         Jan  2020
+#
+#        Homework 1, problem 6
+#
+
+from sympy import *
+#from sympy.physics.quantum import Commutator, Dagger, Operator
+#from sympy.physics.quantum.operator import DifferentialOperator
+
+#==============================================================================
+# time-independent symbolic variables
+#==============================================================================
+#
+L = symbols(' L ',real=True, positive=True)
+x, t= symbols(' x t ', real=True)
+n, m  = symbols('n m', integer=True, positive=True)  # note this must be integer, not int
+M = symbols(' M ',real=True, positive= True)
+h_bar = symbols(' h_bar ',real=True,positive=true)
+#f = Function('f')
+#d = DifferentialOperator(Derivative(f(x), x), f(x))
+
+#==============================================================================
+# time-independent wave functions
+#==============================================================================
+#
+def psi(n,L,x):
+    result = sqrt(2/L)*sin(n*pi*x/L)
+    return result
+
+#==============================================================================
+# commutation operation
+#==============================================================================
+#
+comm = (h_bar / 1.0j) * (x * diff(psi(n,L,x),x,1) - diff(x * psi(n,L,x),x,1))
+#comm = Commutator(x, d) * psi(n,L,x)
+
+#==============================================================================
+# show or print result
+#==============================================================================
+#
+pprint(comm)
+
+
